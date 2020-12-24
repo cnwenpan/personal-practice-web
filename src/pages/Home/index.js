dbe94975b60a642383d7a95f72cd017ae1ee4c9f
wenpan <cnwenpan@gmail.com>
2020-12-24 22:38:22 +0800

    首页优化，项目筛选非重复任务

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,20 @@
 import React, {Component} from 'react';
-import {Card, Row, Col} from 'antd'
+import {Card, Row, Col, Tag} from 'antd'
 import moment from 'moment'
 import InfoBlock from "@/components/InfoBlock";
 import CheckList from '@/components/CheckList'
 import Api from './api'
 
+const {CheckableTag} = Tag;
+
 class Home extends Component {
     state = {
         data: [],
-        unRepeatData:[]
+        unRepeatData: [],
+        selectedTags: [],
+        programs: []
+
+
     }
 
     componentDidMount() {
@@ -20,13 +26,23 @@ class Home extends Component {
         Api.today().then(res => {
             this.setState({
                 data: res.repeatResult,
-                unRepeatData:res.notRepeatResult
+                unRepeatData: res.notRepeatResult,
+                programs: [...new Set(res.notRepeatResult.map(item => item.programName))]
             })
         })
     }
 
+
+    handleTagChange = (row, value) => {
+        const {selectedTags} = this.state;
+        const nextSelectedTags = value ? [...selectedTags, row] : selectedTags.filter(t => t !== row);
+
+        this.setState({selectedTags: nextSelectedTags});
+    }
+
+
     render() {
-        const {data,unRepeatData} = this.state;
+        const {data, unRepeatData, selectedTags, programs} = this.state;
         const unDoTaskList = data.filter(item => !item.status);
         let restTime = 0;
         unDoTaskList.forEach(item => {
@@ -35,8 +51,8 @@ class Home extends Component {
         return (
             <div>
                 <Card>
-                    <div style={{marginBottom:10,color:'#345753'}}>重复任务</div>
-                    <Row gutter={10} style={{marginBottom:10}}>
+                    <div style={{marginBottom: 10, color: '#345753'}}>重复任务</div>
+                    <Row gutter={10} style={{marginBottom: 10}}>
                         <Col>
                             <InfoBlock label="剩余时长" value={restTime}
                             />
@@ -48,9 +64,22 @@ class Home extends Component {
 
                     <CheckList data={data} onRefresh={this.queryTodayTasks}/>
                 </Card>
-                <Card style={{marginTop:10}}>
-                    <div style={{marginBottom:10,color:'#345753'}}>不重复任务</div>
-                    <CheckList data={unRepeatData} type={1} onRefresh={this.queryTodayTasks}/>
+                <Card style={{marginTop: 10}}>
+                    <div style={{marginBottom: 10, color: '#345753'}}>
+
+                        <span style={{marginRight:20}}>不重复任务</span>
+                        {programs.map(item => (<CheckableTag
+                            key={item}
+                            checked={selectedTags.indexOf(item) > -1}
+                            onChange={checked => this.handleTagChange(item, checked)}
+                        >
+                            {item}
+                        </CheckableTag>))}
+                    </div>
+
+                    <CheckList
+                        data={selectedTags.length === 0 ? unRepeatData : unRepeatData.filter(item => selectedTags.indexOf(item.programName) > -1)}
+                        type={1} onRefresh={this.queryTodayTasks}/>
                 </Card>
             </div>
         );
